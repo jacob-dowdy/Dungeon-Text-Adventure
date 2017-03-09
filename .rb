ce1da158d5cbed6f1b42614eaebe31a95d107a1f
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-03-09 15:17:42 -0500

    Create .rb

---
+++ .rb
@@ -0,0 +1,27 @@
+# Dungeon Text Adventure!
+class Dungeon
+  attr_accessor :player
+  def initializer(player)
+    @player = player
+    @rooms = {}
+  end
+end
+
+class Player
+  attr_accessor :name, :location
+  
+  def initialize(player_name)
+    @name = player_name
+  end
+end
+
+class Room
+  attr_accessor :reference, :name, :description, :connections
+  
+  def initialize(reference, name, description, connections)
+    @reference = reference
+    @name = name
+    @description = description
+    @connections = connections
+  end
+end
