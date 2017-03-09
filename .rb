26e6e15e53c078e2ca3cf17b249386e21733ceea
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-03-09 15:53:38 -0500

    Update .rb

--- .rb
+++ .rb
@@ -10,3 +10,14 @@ end
 
 Player = Struct.new(:name, :location)
 Room = Struct.new(:reference, :name, :description, :connections)
+
+class Dungeon
+  def add_room(reference, name, description, connections)
+    @rooms[reference] = Room.new(reference, name, description, connections)
+  end
+end
+
+my_dungeon.add_room(:largecave, "Large Cave", "a large cavernous cave", { :west => :smallcave })
+
+my_dungeon.add_room(:smallcave, "Small Cave", "a small, claustrophobic cave", { :east => :largecave})
+
